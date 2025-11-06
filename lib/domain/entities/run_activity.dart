66ae90bc0f748acf669d889c4549c8f98761564d
MinhDeepZaiZLer <nguyengiaminh101105@gmail.com>
2025-11-06 14:54:37 +0700

    define location_point and run_activity

---
+++ lib/domain/entities/run_activity.dart
@@ -0,0 +1,23 @@
+import 'location_point.dart';
+
+class RunActivity {
+  final String id;
+  final String userId;
+
+  final List<LocationPoint> router; // luu lai toa do gps -> ve ra duong chay
+  final int durationInSeconds; //tong thoi gian chay tinh bang giay
+  final double distanceInMeters; // tong quang duong chay duoc tinh bang met
+  final DateTime timestamp; // thoi diem bat dau chay
+  final double avgSpeedKmh; // Tốc độ trung bình (km/h)
+  final double caloriesBurned;
+  RunActivity({
+    required this.id,
+    required this.userId,
+    required this.router,
+    required this.durationInSeconds,
+    required this.distanceInMeters,
+    required this.timestamp,
+    required this.avgSpeedKmh,
+    required this.caloriesBurned,
+  });
+}
